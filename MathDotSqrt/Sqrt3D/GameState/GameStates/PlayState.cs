203aaf7b964faa2c43f032bb685f3521691f1d96
Chris Trenkov <ctrenkov@gmail.com>
2016-12-08 17:00:05 -0500

    First Official Commit

--- MathDotSqrt/Sqrt3D/GameState/GameStates/PlayState.cs
+++ MathDotSqrt/Sqrt3D/GameState/GameStates/PlayState.cs
@@ -35,58 +35,12 @@ namespace MathDotSqrt.Sqrt3D.GameState.GameStates {
 
 		public override void Init() {
 			scene = new Scene();
-			PerspectiveCamera camera = new PerspectiveCamera(70, Window.ASPECT_RATIO, .01f, 10000);
-			camera.Position.Z = 5;
-			scene.Add(camera);
-
-			Geometry cubeGeometry = OBJLoader.LoadOBJ("cube.obj");
-			Material skyMaterial = new MeshSkyboxMaterial() {
-				SkyboxCubeMap = TextureLoader.LoadCubeMap("lagoon", true)
-			};
-			Mesh skyBox = new Mesh(cubeGeometry, skyMaterial);
-			skyBox.SetScale(9000);
-			scene.Add(skyBox);
-
-			Geometry levelGeometry = OBJLoader.LoadOBJ("level.obj");
-			Material levelMaterial = new MeshSpecularMaterial() {
-				CubeMapReflection = TextureLoader.LoadCubeMap("lagoon", true),
-			};
-			Mesh mesh = new Mesh(levelGeometry, levelMaterial);
-			scene.Add(mesh);
-
-			Light ambientLight = new AmbientLight(Color.Silver, 1f);
-			scene.Add(ambientLight);
-			Light pointLight = new PointLight(Color.Silver, 1f);
-			camera.Add(pointLight);
-			scene.Add(pointLight);
-
-
-			//--GUI--//
+			
 			gui = new GuiField();
 		}
 
-		float count = 0;
-
 		public override void Update(float delta) {
-			//scene.Camera.Position.Z = ( (float)Math.Sin(count) * 1000 ) + 500;
-
-			//count += .005f;
-
 			
-
-			if(Keyboard.GetState().IsKeyDown(Key.D))
-				scene.Camera.RotateY(1.5f);
-			if(Keyboard.GetState().IsKeyDown(Key.A))
-				scene.Camera.RotateY(-1.5f);
-			if(Keyboard.GetState().IsKeyDown(Key.Escape))
-				Environment.Exit(0);
-
-			if(Keyboard.GetState().IsKeyDown(Key.W)) {
-				Output.Good(scene.Camera.Rotation.Y % 360);
-				float rad = SexyMathHelper.ToRad(scene.Camera.Rotation.Y);
-				scene.Camera.Position.X += (float)Math.Sin(rad);
-				scene.Camera.Position.Z += (float)-Math.Cos(rad);
-			}
 		}
 		public override void Render(OpenGLRenderer renderer) {
 			renderer.RenderScene(scene);
@@ -96,10 +50,6 @@ namespace MathDotSqrt.Sqrt3D.GameState.GameStates {
 
 		}
 		public override void UpdateResize() {
-			PerspectiveCamera camera = (PerspectiveCamera)scene.GetCamera();
-			camera.AspectRatio = Window.ASPECT_RATIO;
-			camera.UpdateProjectionMatrix();
-
 			gui.UpdateScreenSize();
 		}
 
